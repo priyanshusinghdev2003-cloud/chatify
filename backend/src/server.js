035ffbaa327af4ebb47121bb903f7d8db78406dd
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2025-10-31 12:47:17 +0530

    "ipdates some changes"

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,13 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/db.js";
 import authRouter from "./routes/auth.route.js"
+import path from "path"
 
 dotenv.config()
 
 const app = express();
 const port  = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 
 app.get("/api/test", (req,res)=>{
@@ -15,6 +17,14 @@ app.get("/api/test", (req,res)=>{
 })
 
 app.use("/api/auth", authRouter)
+
+// make ready for deployment
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+  })
+}
 
 connectDB().then(()=>{
   app.listen(port, () => {
